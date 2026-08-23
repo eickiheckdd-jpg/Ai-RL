b544eb40ee54d52a0b8ffae476824375698fa1dd
eickiheckdd-jpg <eickiheckdd@gmail.com>
2026-08-23 21:05:28 +0800

    Update PPOAgent.java

--- src/main/java/com/example/newgen6/PPOAgent.java
+++ src/main/java/com/example/newgen6/PPOAgent.java
@@ -1,11 +1,11 @@
 package com.example.newgen6;
 
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.WorkspaceMode;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.memory.MemoryWorkspace;
@@ -16,40 +16,36 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class PPOAgent {
     private static PPOAgent instance;
-    private MultiLayerNetwork network;
+    private ComputationGraph network;
 
-    public static class InferenceResult {
-        public float yawDelta;
-        public float pitchDelta;
-
-        public InferenceResult(float yawDelta, float pitchDelta) {
-            this.yawDelta = yawDelta;
-            this.pitchDelta = pitchDelta;
-        }
-    }
+    // A Java 21 Record automatically generates the yawDelta() and pitchDelta() methods for you
+    public record InferenceResult(float yawDelta, float pitchDelta) {}
 
     private PPOAgent() {
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+        // Rebuilt as a ComputationGraph to satisfy PPOTrainerThread
+        ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(12345)
                 .updater(new Adam(RLConfig.LEARNING_RATE))
                 .trainingWorkspaceMode(WorkspaceMode.ENABLED)
                 .inferenceWorkspaceMode(WorkspaceMode.ENABLED)
-                .list()
-                .layer(0, new DenseLayer.Builder()
+                .graphBuilder()
+                .addInputs("input")
+                .addLayer("dense1", new DenseLayer.Builder()
                         .nIn(RLConfig.OBS_DIM)
                         .nOut(16)
                         .activation(Activation.RELU)
                         .weightInit(WeightInit.XAVIER)
-                        .build())
-                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                        .build(), "input")
+                .addLayer("output", new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
                         .nIn(16)
                         .nOut(RLConfig.ACTION_CONTINUOUS_DIM)
                         .activation(Activation.TANH)
                         .weightInit(WeightInit.XAVIER)
-                        .build())
+                        .build(), "dense1")
+                .setOutputs("output")
                 .build();
 
-        network = new MultiLayerNetwork(conf);
+        network = new ComputationGraph(conf);
         network.init();
     }
 
@@ -60,7 +56,7 @@ public class PPOAgent {
         return instance;
     }
 
-    public MultiLayerNetwork getNetwork() {
+    public ComputationGraph getNetwork() {
         return network;
     }
 
@@ -70,10 +66,10 @@ public class PPOAgent {
         
         try (MemoryWorkspace ws = Nd4j.getWorkspaceManager().getAndActivateWorkspace("MOBILE_AI_INFERENCE")) {
             INDArray input = Nd4j.create(obs).reshape(1, RLConfig.OBS_DIM);
-            INDArray output = network.output(input, false);
+            INDArray[] output = network.output(false, input);
             
-            yaw = output.getFloat(0, 0) * RLConfig.MAX_YAW_DELTA;
-            pitch = output.getFloat(0, 1) * RLConfig.MAX_PITCH_DELTA;
+            yaw = output[0].getFloat(0, 0) * RLConfig.MAX_YAW_DELTA;
+            pitch = output[0].getFloat(0, 1) * RLConfig.MAX_PITCH_DELTA;
         }
         
         return new InferenceResult(yaw, pitch);
