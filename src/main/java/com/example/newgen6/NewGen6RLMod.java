3d4f305331662bffca8cf086ff28178f6e8973ba
eickiheckdd-jpg <eickiheckdd@gmail.com>
2026-08-23 21:04:59 +0800

    Update NewGen6RLMod.java

--- src/main/java/com/example/newgen6/NewGen6RLMod.java
+++ src/main/java/com/example/newgen6/NewGen6RLMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,9 +23,9 @@ public class NewGen6RLMod implements ModInitializer {
 
             float[] obs = getPlayerObservations(client);
             PPOAgent.InferenceResult action = PPOAgent.getInstance().predict(obs);
-
-            client.player.setYaw(client.player.getYaw() + action.yawDelta);
-            client.player.setPitch(client.player.getPitch() + action.pitchDelta);
+            
+            // Route the result to your custom CombatActionExecutor
+            CombatActionExecutor.execute(client.player, action);
         });
     }
 
@@ -34,15 +33,23 @@ public class NewGen6RLMod implements ModInitializer {
         float[] obs = new float[RLConfig.OBS_DIM];
         if (client.player == null) return obs;
 
-        Vec3d pPos = client.player.getPos();
+        // Direct coordinate access avoids all Yarn mapping getPos() conflicts
+        double px = client.player.getX();
+        double py = client.player.getY();
+        double pz = client.player.getZ();
+
         Entity target = null;
         double closestDist = 30.0;
 
         if (client.world != null) {
             for (Entity e : client.world.getEntities()) {
                 if (e != client.player && e.isAlive()) {
-                    Vec3d ePos = e.getPos();
-                    double dist = pPos.distanceTo(ePos);
+                    double ex = e.getX();
+                    double ey = e.getY();
+                    double ez = e.getZ();
+                    
+                    // Manual distance calculation
+                    double dist = Math.sqrt(Math.pow(ex - px, 2) + Math.pow(ey - py, 2) + Math.pow(ez - pz, 2));
                     if (dist < closestDist) {
                         closestDist = dist;
                         target = e;
@@ -52,10 +59,9 @@ public class NewGen6RLMod implements ModInitializer {
         }
 
         if (target != null) {
-            Vec3d tPos = target.getPos();
-            obs[0] = (float) (tPos.x - pPos.x);
-            obs[1] = (float) (tPos.y - pPos.y);
-            obs[2] = (float) (tPos.z - pPos.z);
+            obs[0] = (float) (target.getX() - px);
+            obs[1] = (float) (target.getY() - py);
+            obs[2] = (float) (target.getZ() - pz);
             obs[3] = client.player.getPitch();
             obs[4] = client.player.getYaw();
             obs[5] = (float) closestDist;
