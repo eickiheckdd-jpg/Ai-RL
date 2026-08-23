10324e09176ce0b908600ae07f904c378ff00038
eickiheckdd-jpg <eickiheckdd@gmail.com>
2026-08-23 14:21:33 +0800

    Update ActorNetwork.java

--- src/main/java/com/example/newgen6/ActorNetwork.java
+++ src/main/java/com/example/newgen6/ActorNetwork.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class ActorNetwork {
     private final int inputSize, outputSize;
     private final int hidden = 64;
-    private final float[] w1, b1, w2, b2;
+    public final float[] w1, b1, w2, b2; // Changed to public
 
     public ActorNetwork(int inputSize, int outputSize) {
         this.inputSize = inputSize;
