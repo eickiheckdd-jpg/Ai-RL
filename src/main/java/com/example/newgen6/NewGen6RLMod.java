a0e425780a71594ae4fc5bc80253a6dc5eb17cde
eickiheckdd-jpg <eickiheckdd@gmail.com>
2026-08-25 07:18:36 +0800

    Create NewGen6RLMod.java

---
+++ src/main/java/com/example/newgen6/NewGen6RLMod.java
@@ -0,0 +1,10 @@
+package com.example.newgen6;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NewGen6RLMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[NewGen6] Client RL Engine Loaded Successfully!");
+    }
+}
