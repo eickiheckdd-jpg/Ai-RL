84132b18f6511486284e7707da4521694dab8ab9
eickiheckdd-jpg <eickiheckdd@gmail.com>
2026-08-24 20:47:33 +0800

    Update MLP.java

--- src/main/java/com/example/newgen6/rl/MLP.java
+++ src/main/java/com/example/newgen6/rl/MLP.java
@@ -8,20 +8,17 @@ public class MLP implements Serializable {
 
     public float[][] weights1, weights2, weights3;
     public float[] bias1, bias2, bias3;
-    public transient float[] hidden1, hidden2;
     private final Random rand = new Random();
 
     public MLP(int in, int h, int out) {
         weights1 = new float[in][h]; bias1 = new float[h];
         weights2 = new float[h][h];  bias2 = new float[h];
         weights3 = new float[h][out]; bias3 = new float[out];
-        hidden1 = new float[h]; hidden2 = new float[h];
         initWeights();
     }
 
     public void initTransient() {
-        if (hidden1 == null) hidden1 = new float[bias1.length];
-        if (hidden2 == null) hidden2 = new float[bias2.length];
+        // No longer relying on global transient hidden state arrays
     }
 
     private void initWeights() {
@@ -30,11 +27,13 @@ public class MLP implements Serializable {
         for (int i = 0; i < weights3.length; i++) for (int j = 0; j < weights3[0].length; j++) weights3[i][j] = (rand.nextFloat() - 0.5f) * 0.1f;
     }
 
-    public void forward(float[] input, float[] output) {
-        initTransient();
-        computeLayer(input, weights1, bias1, hidden1, true);
-        computeLayer(hidden1, weights2, bias2, hidden2, true);
-        computeLayer(hidden2, weights3, bias3, output, false);
+    public float[] forward(float[] input, float[] output) {
+        float[] h1 = new float[bias1.length];
+        float[] h2 = new float[bias2.length];
+        computeLayer(input, weights1, bias1, h1, true);
+        computeLayer(h1, weights2, bias2, h2, true);
+        computeLayer(h2, weights3, bias3, output, false);
+        return h2; // Returns thread-safe hidden layer 2 activations for backprop
     }
 
     private void computeLayer(float[] in, float[][] w, float[] b, float[] out, boolean relu) {
