b33ad17f8b754e7eb10e9a12fb76e8089d392089
eickiheckdd-jpg <eickiheckdd@gmail.com>
2026-08-24 15:15:22 +0800

    Update Matrix.java

--- src/main/java/com/example/newgen6/rl/Matrix.java
+++ src/main/java/com/example/newgen6/rl/Matrix.java
@@ -22,4 +22,29 @@ public class Matrix {
         }
         return m;
     }
+
+    // Performs per-parameter adaptive updates using Adam momentum tracking
+    public void adamUpdate(Matrix grad, Matrix m, Matrix v, int t, float lr, float beta1, float beta2, float eps) {
+        float b1Correction = 1.0f - (float) Math.pow(beta1, t);
+        float b2Correction = 1.0f - (float) Math.pow(beta2, t);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                float g = grad.data[i][j];
+                
+                // First moment vector update
+                m.data[i][j] = beta1 * m.data[i][j] + (1.0f - beta1) * g;
+                
+                // Second moment vector update
+                v.data[i][j] = beta2 * v.data[i][j] + (1.0f - beta2) * (g * g);
+
+                // Bias corrections
+                float mHat = m.data[i][j] / b1Correction;
+                float vHat = v.data[i][j] / b2Correction;
+
+                // Gradient update
+                this.data[i][j] -= lr * mHat / ((float) Math.sqrt(vHat) + eps);
+            }
+        }
+    }
 }
